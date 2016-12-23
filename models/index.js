4869c7833ae4d1c04ddada5d7f610354e5e282a6
git-clay <ce.smith5525@gmail.com>
2016-12-23 11:53:22 -0700

    stuff

---
+++ models/index.js
@@ -0,0 +1,6 @@
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI ||process.env.MONGOLAB_URI||
+	process.env.MONGOHQ_URL ||"mongodb://localhost/");
+
+// module.exports.User = require('./user.js');
+// module.exports.DailyInfo=require('./dailyInfo');
---
+++ server.js
@@ -0,0 +1,33 @@
+var express		= require('express'),
+	bodyParser	= require('body-parser'),
+	app			= express(),
+	db			= require('./models');
+	
+// serve static files from public folder
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+/*********************** VIEWS ******************************/
+app.set('views', './views');
+app.engine('ejs', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+app.get('/', function homepage (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+/*********************** ROUTES ******************************/
+var routes = require('./routes/routes.js');
+app.use(routes);
+
+
+
+
+
+
+// listen on port 3000
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server is running on http://localhost:3000/');
+});
